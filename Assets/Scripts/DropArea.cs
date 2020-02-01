e7739b495e033b1d90f509cd76dcdaeb868bf265
Leo <leojh.li@mail.utoronto.ca>
2020-02-01 15:22:25 -0500

    Gathering complete

--- Assets/Scripts/DropArea.cs
+++ Assets/Scripts/DropArea.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class DropArea : MonoBehaviour
 {
-	MouseBehaviour mouse;
-    // Start is called before the first frame update
-    void Start()
-    {
-		mouse = Camera.main.GetComponent<MouseBehaviour>();
-    }
 	
-	public bool Drop(Draggable thing){
-		// TODO
-		// Debug.Log("Dropped a " + thing.gameObject.name + " into " + this.gameObject.name);
-		return true;
-	}
 }
