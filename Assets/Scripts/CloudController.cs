bf956e2a04e12a8673fc2728fcc542a27dda07c8
Leo <leojh.li@mail.utoronto.ca>
2020-02-02 04:26:04 -0500

    More cloud stuff

--- Assets/Scripts/CloudController.cs
+++ Assets/Scripts/CloudController.cs
@@ -6,6 +6,8 @@ public class CloudController : MonoBehaviour {
     public Transform[] m_cloudSets;
     public float m_scrollSpeed;
     public float m_parallaxFactor;
+    [Tooltip("Distance to place cloud sets apart.")]
+    public float m_cloudSetDistance;
     private CameraControl camControl;
     private float m_currCameraScroll;
 
@@ -16,6 +18,11 @@ public class CloudController : MonoBehaviour {
     private void Update() {
         foreach (Transform cloudSet in m_cloudSets) {
             cloudSet.position += Vector3.right * (m_scrollSpeed * Time.deltaTime - m_currCameraScroll *  1 / m_parallaxFactor);
+            // Move cloud set to left if necessary
+            if (cloudSet.position.x >= camControl.rightBound + m_cloudSetDistance) {
+                Debug.Log("true");
+                cloudSet.position = transform.position - Vector3.right * m_cloudSetDistance * Mathf.Floor(m_cloudSets.Length / 2f);
+            }
         }
     }
 
