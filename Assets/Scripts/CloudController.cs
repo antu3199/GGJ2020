1dffecd5960015601184c0b78e6e66806777e923
Leo <lileo.jh@gmail.com>
2020-02-02 06:09:33 -0500

    Clouds complete

--- Assets/Scripts/CloudController.cs
+++ Assets/Scripts/CloudController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CloudController : MonoBehaviour {
     public Transform[] m_cloudSets;
     public float m_scrollSpeed;
+    [Tooltip("Value that dampens the effect of the camera scroll on the clouds' movement.")]
     public float m_parallaxFactor;
     [Tooltip("Distance to place cloud sets apart.")]
     public float m_cloudSetDistance;
@@ -13,15 +14,18 @@ public class CloudController : MonoBehaviour {
 
     private void Start() {
         camControl = Camera.main.GetComponent<CameraControl>();
+        if (camControl == null) {
+            Debug.LogWarning("CameraControl not found. Must be enabled for cloud parallaxing.");
+        }
     }
 
     private void Update() {
-        foreach (Transform cloudSet in m_cloudSets) {
-            cloudSet.position += Vector3.right * (m_scrollSpeed * Time.deltaTime - m_currCameraScroll *  1 / m_parallaxFactor);
-            // Move cloud set to left if necessary
-            if (cloudSet.position.x >= camControl.rightBound + m_cloudSetDistance) {
-                Debug.Log("true");
-                cloudSet.position = transform.position - Vector3.right * m_cloudSetDistance * Mathf.Floor(m_cloudSets.Length / 2f);
+        for (int i = m_cloudSets.Length - 1; i >= 0; i--) {
+            m_cloudSets[i].position += Vector3.right * (m_scrollSpeed * Time.deltaTime - m_currCameraScroll * -m_parallaxFactor);
+            // Loop cloud set back to left if necessary
+            if (m_cloudSets[i].position.x >= camControl.rightBound + m_cloudSetDistance * m_cloudSets.Length / 2) {
+                Transform nextSet = m_cloudSets[(i + 1) % m_cloudSets.Length];
+                m_cloudSets[i].position = nextSet.position - Vector3.right * m_cloudSetDistance;
             }
         }
     }
