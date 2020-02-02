19c57f25f63b17d3e94b96b07a4d32ebed7e8b99
Yilun Zhao <yl4zhao@uwaterloo.ca>
2020-02-02 13:37:10 -0500

    Adding inventory hook

--- Assets/Scripts/SummonerInventory.cs
+++ Assets/Scripts/SummonerInventory.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class SummonerInventory : MonoBehaviour {
-	public Dictionary<Resource, int> m_resources = new Dictionary<Resource, int>();
-
-	// Use this for initialization
-	void Start () {
-		m_resources.Add(Resource.ORE, 0);
-		m_resources.Add(Resource.WOOD, 0);
-		m_resources.Add(Resource.WHEAT, 0);
-		m_resources.Add(Resource.GOLD, 0);
+	[Serializable]
+	public class ResourceStack {
+		public Text m_ui;
+		public int m_count;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	[Serializable]
+	public class Entry {
+		public Resource m_resource;
+		public ResourceStack m_stack;
+	}
+
+	public List<Entry> m_entries = new List<Entry>();
+	private Dictionary<Resource, ResourceStack> m_resources = new Dictionary<Resource, ResourceStack>();
+
+	void Start() {
+		foreach (Entry entry in m_entries) {
+			m_resources.Add(entry.m_resource, entry.m_stack);
+			updateResource(entry.m_resource, 0);
+		}
 	}
 
 	public int getResource(Resource resource) {
 		if(m_resources.ContainsKey(resource)) {
-			return m_resources[resource];
+			return m_resources[resource].m_count;
 		}
 
 		else {
@@ -29,30 +37,35 @@ public class SummonerInventory : MonoBehaviour {
 		}
 	}
 
+	private void updateResource(Resource resource, int amount) {
+		m_resources[resource].m_count += amount;
+		m_resources[resource].m_ui.text = m_resources[resource].m_count.ToString();
+	}
+
 	public bool tryConsumeResource(Resource resource, int amount) {
-		if(!m_resources.ContainsKey(resource) || m_resources[resource] < amount) {
+		if(!m_resources.ContainsKey(resource) || m_resources[resource].m_count < amount) {
 			return false;
 		}
 
-		m_resources[resource] -= amount;
+		updateResource(resource, -1*amount);
 		return true;
 	}
 
 	public bool tryConsumeResources(Dictionary<Resource, int> requirements) {
 		foreach(KeyValuePair<Resource, int> resource in requirements) {
-			if(!m_resources.ContainsKey(resource.Key) || m_resources[resource.Key] < resource.Value) {
+			if(!m_resources.ContainsKey(resource.Key) || m_resources[resource.Key].m_count < resource.Value) {
 				return false;
 			}
 		}
 
 		foreach(KeyValuePair<Resource, int> resource in requirements) {
-			m_resources[resource.Key] -= resource.Value;
+			updateResource(resource.Key, -1*resource.Value);
 		}
 
 		return true;
 	}
 
 	public void addResource(Resource resource, int amount) {
-		m_resources[resource] += amount;
+		updateResource(resource, amount);
 	}
 }
