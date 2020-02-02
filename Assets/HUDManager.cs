4962cec4060eb79a5cea72e40ad8facf24c7ff47
Anthony Tu <antu3199@gmail.com>
2020-02-01 22:26:26 -0500

    Working HUD

--- Assets/HUDManager.cs
+++ Assets/HUDManager.cs
@@ -1,22 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HUDManager : MonoBehaviour
+public enum HUDState {
+  BASE,
+  BUILD,
+
+};
+
+public class HUDManager : Singleton<HUDManager>
 {
+    string tooltipCreateWall = "Create a wall (Cost: 15 ore)";
+    string tooltipCreateTurret = "Create a turret (Cost: 30 ore, 10 wood, 10 wheat)";
+
+    string tooltipBuild = "Place the structure";
+
+    public HUDState hudState;
+    public Text hudText;
+
+    public HorizontalLayoutGroup leftRightItems;
+    public RectTransform cancelButton;
+
+    public BuildableObject wallObject;
+    public BuildableObject turretObject;
 
 
-  
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hudState = HUDState.BASE;
+        RefreshHUDSDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public void BuildWall() {
+      if (hudState != HUDState.BASE) {
+        return;
+      }
+
+      hudState = HUDState.BUILD;
+      Instantiate(wallObject); // Will position itself automatically
+      RefreshHUDSDisplay();
+    }
+
+    public void BuildTurret() {
+      if (hudState != HUDState.BASE) {
+        return;
+      }
+      
+      hudState = HUDState.BUILD;
+
+      Instantiate(turretObject); // Will position itself automatically
+      RefreshHUDSDisplay();
+
+    }
+
+    public void DisplayBuildWallTooltip() {
+      hudText.text = tooltipCreateWall;
+    }
+
+    public void DisplayBuildTurretTooltip() {
+      hudText.text = tooltipCreateTurret;
+    }
+
+    public void PointerExitIcon() {
+      hudText.text = "";
+    }
+
+    public void CancelPressed() {
+      ResetHUDState();
+    }
+
+    public void ResetHUDState() {
+      hudState = HUDState.BASE;
+      hudText.text = "";
+      RefreshHUDSDisplay();
+    }
+
+    public void RefreshHUDSDisplay() {
+      switch (hudState) {
+        case HUDState.BASE:
+          cancelButton.gameObject.SetActive(false);
+          leftRightItems.gameObject.SetActive(true);
+          hudText.text = "";
+          break;
+        case HUDState.BUILD:
+          cancelButton.gameObject.SetActive(true);
+          leftRightItems.gameObject.SetActive(false);
+          hudText.text = tooltipBuild;
+          break;
+        default:
+          break;
+      }
     }
 }
